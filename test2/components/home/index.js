ea1c1092413766a2f4b2676f45afe8c78680cded
Harry Haller <anonacct@comcast.net>
2016-02-27 21:26:49 +0000

    Changes made by Harry Haller (anonacct@comcast.net).

--- test2/components/home/index.js
+++ test2/components/home/index.js
@@ -1,104 +1,61 @@
 'use strict';
+var viewModel = {};
 
 app.home = kendo.observable({
-    onShow: function() {},
-    afterShow: function() {}
+    onShow: function() {
+    },
+    afterShow: function() {
+    }
 });
 
-// START_CUSTOM_CODE_home
-// Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
 
-// END_CUSTOM_CODE_home
-(function(parent) {
-    var provider = app.data.test2,
-        mode = 'signin',
-        registerRedirect = 'home',
-        signinRedirect = 'home',
-        init = function(error) {
-            if (error) {
-                if (error.message) {
-                    alert(error.message);
-                }
-                return false;
-            }
+(function (parent) {
 
-            var activeView = mode === 'signin' ? '.signin-view' : '.signup-view';
+    viewModel = kendo.observable({
+        demoData: [{
+            id: 1,
+            HOTSTAMP: '1234',
+			CARDFORMAT: "26 bit wiegand",
+			STATUS: "ACTIVE",
+            PANELS: [{
+                PANELNAME: "MAIN OFFICE", 
+                ENABLED: "ENABLED",
+                USER_CODE: "1234",
+				PROFILE: 1,
+				PROFILENAME: "MASTER ACCESS",
+            },{
+                PANELNAME: "WAREHOUSE",
+                ENABLED: "DISABLED",
+                USER_CODE: "1234",
+				PROFILE: 0,
+				PROFILENAME: "",
+            }],
+       	},{
+            id: 2,
+            HOTSTAMP: '2222',
+			CARDFORMAT: "26 bit wiegand",
+			STATUS: "ACTIVE",
+            PANELS: [{
+                PANELNAME: "MAIN OFFICE",
+                ENABLED: "ENABLED",
+				USER_NO: "",
+                USER_CODE: "2222",
+				PROFILE: 99,
+				PROFILENAME: "EMPLOYEE"
+            },{
+				PANELNAME: "WAREHOUSE",
+                ENABLED: "ENABLED",
+				USER_NO: "",
+				USER_CODE: "2222",
+				PROFILE: 1,
+				PROFILENAME: "MASTER ACCESS"
+			}],
+        }],
+    	change: function(e){
+        	alert(e.field);
+    	}
+   	});
 
-            if (provider.setup && provider.setup.offlineStorage && !app.isOnline()) {
-                $('.offline').show().siblings().hide();
-            } else {
-                $(activeView).show().siblings().hide();
-            }
-        },
-        successHandler = function(data) {
-            var redirect = mode === 'signin' ? signinRedirect : registerRedirect;
+    parent.set('viewModel', viewModel);
 
-            if (data && data.result) {
-                app.user = data.result;
-
-                setTimeout(function() {
-                    app.mobileApp.navigate('components/' + redirect + '/view.html');
-                }, 0);
-            } else {
-                init();
-            }
-        },
-        homeModel = kendo.observable({
-            displayName: '',
-            email: '',
-            password: '',
-            validateData: function(data) {
-                if (!data.email) {
-                    alert('Missing email');
-                    return false;
-                }
-
-                if (!data.password) {
-                    alert('Missing password');
-                    return false;
-                }
-
-                return true;
-            },
-            signin: function() {
-                var model = homeModel,
-                    email = model.email.toLowerCase(),
-                    password = model.password;
-
-                if (!model.validateData(model)) {
-                    return false;
-                }
-                provider.Users.login(email, password, successHandler, init);
-            },
-            register: function() {
-                var model = homeModel,
-                    email = model.email.toLowerCase(),
-                    password = model.password,
-                    displayName = model.displayName,
-                    attrs = {
-                        Email: email,
-                        DisplayName: displayName
-                    };
-
-                if (!model.validateData(model)) {
-                    return false;
-                }
-
-                provider.Users.register(email, password, attrs, successHandler, init);
-            },
-            toggleView: function() {
-                mode = mode === 'signin' ? 'register' : 'signin';
-                init();
-            }
-        });
-
-    parent.set('homeModel', homeModel);
-    parent.set('afterShow', function() {
-        provider.Users.currentUser().then(successHandler, init);
-    });
 })(app.home);
-
-// START_CUSTOM_CODE_homeModel
-// Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
-
-// END_CUSTOM_CODE_homeModel
---
+++ test2/components/test/index.js
@@ -0,0 +1,154 @@
+'use strict';
+var DMP_GetUserCodes_DataSource_Options = {};
+var DMP_GetUserCodes_DataSource = {};
+var DMP_GetUserCodes_ViewModel = {};
+
+var DMP_GetUserCodes_DataSource_Options2 = {};
+var DMP_GetUserCodes_DataSource2 = {};
+var DMP_GetUserCodes_ViewModel2 = {};
+
+var test = [];
+
+app.home = kendo.observable({
+    onShow: function() {
+        DMP_GetUserCodes_DataSource2.read();
+        DMP_GetUserCodes_DataSource.read();
+        alert(DMP_GetUserCodes_DataSource2.view()[0].CREDENTIAL);
+        
+        /*
+        var templateContent2 = $("#myTemplate").html();
+        var template = kendo.template(templateContent2);
+
+        //Create some dummy data
+        var data = [
+            { name: "John", isAdmin: false },
+            { name: "Alex", isAdmin: true }
+        ];
+        var result = kendo.render(template, data); //render the template
+        $("#users").html(result); //append the result to the page
+        */
+        
+        /*
+        var templateContent3 = $("#product-template").html();
+        alert(templateContent3);
+        var template3 = kendo.template(templateContent3);
+        var data5 = DMP_GetUserCodes_DataSource2.data();
+        var result2 = kendo.render(template3, data5); //render the template
+        $("template#1").html(result2); //append the result to the page
+        
+        
+        var templateContent4 = $("#product-template").html();
+        alert(templateContent4);
+        var template4 = kendo.template(templateContent3);
+
+        //Create some dummy data
+        //var data5 = [];
+        //data5[0] = DMP_GetUserCodes_DataSource2.data()[0]
+        //alert(JSON.stringify(data5));
+        var data6 = DMP_GetUserCodes_DataSource2.data();
+        alert(JSON.stringify(data6));
+        var result3 = kendo.render(template4, data6); //render the template
+        $("template#" + DMP_GetUserCodes_DataSource.view()[1].PANEL).html(result3); //append the result to the page
+        */
+    },
+    afterShow: function() {
+    }
+});
+
+
+(function (parent) {
+// START_CUSTOM_CODE_home
+// Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
+
+    //This datasource performs a GET request to the everlive cloud function S2_GetCardFormats
+    DMP_GetUserCodes_DataSource_Options = {
+        transport: {
+            read: function (options) {
+                //var syncSettings = JSON.stringify(options.data);
+                var data = [];
+                var i = 0;
+                for (i = 0; i < 2; i++) {
+                	data.push({
+                    	PANEL: "PANEL" + i,
+                        IDS: i,
+                    });
+          		}
+                options.success(data);
+            }
+        },
+        change: function (e) {
+        },
+        error: function (err) {
+        },
+        schema: {
+            model: {
+                fields: {
+                    'PANEL': {
+                        field: 'PANEL',
+                        defaultValue: ''
+                    },
+                    'IDS': {
+                        field: 'IDS',
+                        defaultValue: ''
+                    },
+                }
+            }
+        }
+    };
+
+    //Create the datasource with options
+    DMP_GetUserCodes_DataSource = new kendo.data.DataSource(DMP_GetUserCodes_DataSource_Options);
+
+    //This datasource performs a GET request to the everlive cloud function S2_GetCardFormats
+    DMP_GetUserCodes_DataSource_Options2 = {
+        transport: {
+            read: function (options) {
+                //var syncSettings = JSON.stringify(options.data);
+                var data = [];
+                var i = 0;
+                for (i = 0; i < 1; i++) {
+                	data.push({
+                    	CREDENTIAL: "CREDENTIAL" + i,
+                    });
+          		}
+                options.success(data);
+            }
+        },
+        change: function (e) {
+        },
+        error: function (err) {
+        },
+        schema: {
+            model: {
+                fields: {
+                    'CREDENTIAL': {
+                        field: 'CREDENTIAL',
+                        defaultValue: ''
+                    },
+                }
+            }
+        }
+    };
+
+    //Create the datasource with options
+    DMP_GetUserCodes_DataSource2 = new kendo.data.DataSource(DMP_GetUserCodes_DataSource_Options2);
+    
+    
+    DMP_GetUserCodes_ViewModel = kendo.observable({
+        dataSource: DMP_GetUserCodes_DataSource
+    });
+
+    
+    DMP_GetUserCodes_ViewModel2 = kendo.observable({
+        dataSource: DMP_GetUserCodes_DataSource2
+    });
+
+    parent.set('DMP_GetUserCodes_ViewModel', DMP_GetUserCodes_ViewModel);
+    parent.set('DMP_GetUserCodes_ViewModel2', DMP_GetUserCodes_ViewModel2);
+
+})(app.home);
+
+// START_CUSTOM_CODE_homeModel
+// Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
+
+// END_CUSTOM_CODE_homeModel
